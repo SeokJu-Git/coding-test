bb721918617c032e98535d4f945acd1d0e4810b4
SeokJu Hwang <43159256+SeokJu-Git@users.noreply.github.com>
2022-02-14 15:04:50 +0900

    Update number_02.py

--- number_02.py
+++ number_02.py
@@ -1,4 +1,7 @@
 '''
+문제 
+- 행렬의 덧셈
+
 문제 설명
 - 행렬의 덧셈은 행과 열의 크기가 같은 두 행렬의 같은 행, 같은 열의 값을 서로 더한 결과가 됩니다. 2개의 행렬 arr1과 arr2를 입력받아, 행렬 덧셈의 결과를 반환하는 함수, solution을 완성해주세요.
 
@@ -20,3 +23,7 @@ def solution(arr1, arr2):
         answer.append(answer_sub)
         answer_sub = []
     return answer
+
+def solution_(arr1, arr2):
+    return [[c+d for c, d in zip(a, b) fr a, b]]
+
