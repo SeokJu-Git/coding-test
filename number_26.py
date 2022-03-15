faa9f119846b41af5d3496ad1d29ccbcee5d509c
SeokJu Hwang <43159256+SeokJu-Git@users.noreply.github.com>
2022-03-15 23:08:26 +0900

    Update number_26.py

--- number_26.py
+++ number_26.py
@@ -28,6 +28,7 @@
 - 입출력 예#3
   3, 2, 6은 10으로 나누어 떨어지지 않습니다. 나누어 떨어지는 원소가 없으므로 [-1]을 리턴합니다.
 '''
+
 def solution(arr, divisor):
     answer = sorted([i for i in arr if i % divisor == 0])
     if answer == []:
